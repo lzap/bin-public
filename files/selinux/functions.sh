0b73912cff306ab8ee83062aabdfcc2cfe191088
Lukas Zapletal <lzap+git@redhat.com>
2014-05-27 17:45:13 +0200

    installation-fvb

---
+++ files/selinux/functions.sh
@@ -0,0 +1,45 @@
+# sefindif - Find interface definitions that have a string that matches the
+# given regular expression
+sefindif() {
+  REGEXP="$1";
+  pushd ${POLICY_LOCATION}/policy/modules > /dev/null 2>&1;
+  for FILE in */*.if;
+  do
+    awk "/(interface\(|template\()/ { NAME=\$NF; P=0 }; /${REGEXP}/ { if (P==0) {P=1; print NAME}; print };" ${FILE} | sed -e "s:^:${FILE}\: :g";
+  done
+  popd > /dev/null 2>&1;
+}
+
+# seshowif - Show the interface definition
+seshowif() {
+  INTERFACE="$1";
+  pushd ${POLICY_LOCATION}/policy/modules > /dev/null 2>&1;
+  for FILE in */*.if;
+  do
+    grep -A 9999 "\(interface(\`${INTERFACE}'\|template(\`${INTERFACE}'\)" ${FILE} | grep -B 9999 -m 1 "^')";
+  done
+  popd > /dev/null 2>&1;
+}
+
+# sefinddef - Find macro definitions that have a string that matches the given
+# regular expression
+sefinddef() {
+  REGEXP="$1";
+  pushd ${POLICY_LOCATION}/policy/support > /dev/null 2>&1;
+  for FILE in *;
+  do
+    awk "BEGIN { P=1; } /(define\(\`[^\`]*\`$)/ { NAME=\$NF; P=0 }; /${REGEXP}/ { if (P==0) {P=1; print NAME}; print };" ${FILE};
+  done
+  popd > /dev/null 2>&1;
+}
+
+# seshowdef - Show the macro definition
+seshowdef() {
+  MACRONAME="$1";
+  pushd ${POLICY_LOCATION}/policy/support > /dev/null 2>&1;
+  for FILE in *.spt;
+  do
+    grep -A 9999 "define(\`${MACRONAME}'" ${FILE} | grep -B 999 -m 1 "')";
+  done
+  popd > /dev/null 2>&1;
+}
