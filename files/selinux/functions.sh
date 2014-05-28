b4b049e856dc126735459ec1fb1a03975175008d
Lukas Zapletal <lzap+git@redhat.com>
2014-05-28 12:11:03 +0200

    Fixed functions.sh

--- files/selinux/functions.sh
+++ files/selinux/functions.sh
@@ -2,7 +2,11 @@
 # given regular expression
 sefindif() {
   REGEXP="$1";
-  pushd ${POLICY_LOCATION}/policy/modules > /dev/null 2>&1;
+  if test -d /usr/share/selinux/devel/include; then
+    pushd /usr/share/selinux/devel/include > /dev/null 2>&1;
+  else
+    pushd ${POLICY_LOCATION}/policy/modules > /dev/null 2>&1;
+  fi
   for FILE in */*.if;
   do
     awk "/(interface\(|template\()/ { NAME=\$NF; P=0 }; /${REGEXP}/ { if (P==0) {P=1; print NAME}; print };" ${FILE} | sed -e "s:^:${FILE}\: :g";
@@ -13,7 +17,11 @@ sefindif() {
 # seshowif - Show the interface definition
 seshowif() {
   INTERFACE="$1";
-  pushd ${POLICY_LOCATION}/policy/modules > /dev/null 2>&1;
+  if test -d /usr/share/selinux/devel/include; then
+    pushd /usr/share/selinux/devel/include > /dev/null 2>&1;
+  else
+    pushd ${POLICY_LOCATION}/policy/modules > /dev/null 2>&1;
+  fi
   for FILE in */*.if;
   do
     grep -A 9999 "\(interface(\`${INTERFACE}'\|template(\`${INTERFACE}'\)" ${FILE} | grep -B 9999 -m 1 "^')";
@@ -25,7 +33,11 @@ seshowif() {
 # regular expression
 sefinddef() {
   REGEXP="$1";
-  pushd ${POLICY_LOCATION}/policy/support > /dev/null 2>&1;
+  if test -d /usr/share/selinux/devel/include; then
+    pushd /usr/share/selinux/devel/include > /dev/null 2>&1;
+  else
+    pushd ${POLICY_LOCATION}/policy/modules > /dev/null 2>&1;
+  fi
   for FILE in *;
   do
     awk "BEGIN { P=1; } /(define\(\`[^\`]*\`$)/ { NAME=\$NF; P=0 }; /${REGEXP}/ { if (P==0) {P=1; print NAME}; print };" ${FILE};
@@ -36,7 +48,11 @@ sefinddef() {
 # seshowdef - Show the macro definition
 seshowdef() {
   MACRONAME="$1";
-  pushd ${POLICY_LOCATION}/policy/support > /dev/null 2>&1;
+  if test -d /usr/share/selinux/devel/include; then
+    pushd /usr/share/selinux/devel/include > /dev/null 2>&1;
+  else
+    pushd ${POLICY_LOCATION}/policy/modules > /dev/null 2>&1;
+  fi
   for FILE in *.spt;
   do
     grep -A 9999 "define(\`${MACRONAME}'" ${FILE} | grep -B 999 -m 1 "')";
